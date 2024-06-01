d50e5a2a35712fe84cb26cc3314b3df64d25b63c
anh vo <anhvth.226@gmail.com>
2024-06-02 01:13:41 +0700

    udpat

--- vim/install.sh
+++ vim/install.sh
@@ -2,4 +2,6 @@
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-nvim + PlugInstall
+#nvim + PlugInstall
+nvim +'PlugInstall --sync' +qa
+
