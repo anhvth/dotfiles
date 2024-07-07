c7dfd8a4a8e759a3236260669ea01418e2655ea0
Anh. Võ Trần Hải (5) <anhvth5@vng.com.vn>
2024-07-07 16:26:49 +0700

    i[date

--- zsh/keybindings.sh
+++ zsh/keybindings.sh
@@ -1,121 +1,94 @@
-# up
-	function up_widget() {
-		BUFFER="cd .."
-		zle accept-line
-	}
-	zle -N up_widget
-	bindkey "^k" up_widget
+# Up one directory
+function up_widget() {
+	BUFFER="cd .."
+	zle accept-line
+}
+zle -N up_widget
+bindkey "^k" up_widget
 
-# git
-	function git_prepare() {
-		if [ -n "$BUFFER" ];
-			then
-				BUFFER="git add -A && git commit -m \"$BUFFER\""
-		fi
+# Git commit preparation
+function git_prepare() {
+	if [ -n "$BUFFER" ]; then
+		BUFFER="git add -A && git commit -m \"$BUFFER\""
+	else
+		BUFFER="git add -A && git commit -v"
+	fi
+	zle accept-line
+}
+zle -N git_prepare
+bindkey "^g" git_prepare
 
-		if [ -z "$BUFFER" ];
-			then
-				BUFFER="git add -A && git commit -v "
-				# BUFFER="git add -A && git commit -v"
-		fi
-				
-		zle accept-line
-	}
-	zle -N git_prepare
-	bindkey "^g" git_prepare
-    
-	function git_sync() {
-        BUFFER="git pull && git add -A && git commit -m 'code sync' "
-    }
-	zle -N git_sync
-	bindkey "^p" git_sync
-# synccode
-    
+# Git sync
+function git_sync() {
+	BUFFER="git pull && git add -A && git commit -m 'code sync'"
+	zle accept-line
+}
+zle -N git_sync
+bindkey "^p" git_sync
 
-# home
-	#function goto_home() { 
-	#	BUFFER="cd ~/"$BUFFER
-	#	zle end-of-line
-	#	zle accept-line
-	#}
-	#zle -N goto_home
-	#bindkey "^h" goto_home
-# search command
-    # function search_cmd(){
-    #     BUFFER="history > .history && python ~/dotfiles/tools/history_grep.py | fzf)"
-	# 	zle accept-line
-    # }
-	# zle -N search_cmd
-	# bindkey "^f" search_cmd
-        
+# Edit and rerun command
+function edit_and_run() {
+	BUFFER="fc"
+	zle accept-line
+}
+zle -N edit_and_run
+bindkey "^v" edit_and_run
 
-# Edit and rerun
-	function edit_and_run() {
-		BUFFER="fc"
-		zle accept-line
-	}
-	zle -N edit_and_run
-	bindkey "^v" edit_and_run
+# Clear screen
+function ctrl_l() {
+	BUFFER="clear"
+	zle accept-line
+}
+zle -N ctrl_l
+bindkey "^l" ctrl_l
 
-# LS
-	function ctrl_l() {
-		BUFFER="clear"
-		zle accept-line
-	}
-	zle -N ctrl_l
-	bindkey "^l" ctrl_l
+# List files
+function ctrl_n() {
+	BUFFER="ls"
+	zle accept-line
+}
+zle -N ctrl_n
+bindkey "^n" ctrl_n
 
-# neovim-terminal
-	function ctrl_n() {
-		BUFFER="ls"
-		zle accept-line
-	}
-	zle -N ctrl_n
-	bindkey "^n" ctrl_n
-# # Enter
-# 	function enter_line() {
-# 		zle accept-line
-# 	}
-# 	zle -N enter_line
-# 	bindkey "^o" enter_line
+# Add sudo to the current command
+function add_sudo() {
+	BUFFER="sudo $BUFFER"
+	zle end-of-line
+}
+zle -N add_sudo
+bindkey "^s" add_sudo
 
-# Sudo
-	function add_sudo() {
-		BUFFER="sudo "$BUFFER
-		zle end-of-line
-	}
-	zle -N add_sudo
-	bindkey "^s" add_sudo
+# Add code-debug to the current command
+function add_code_debug() {
+	BUFFER="code-debug \"$BUFFER\""
+	zle end-of-line
+}
+zle -N add_code_debug
+bindkey "^o" add_code_debug
 
-# code_debug
-	function add_code_debug() {
-		BUFFER="code-debug \""$BUFFER"\""
-		zle end-of-line
-	}
-	zle -N add_code_debug
-	bindkey "^o" add_code_debug
+# Push file using rsync
+function rs_push() {
+	filename=$(ls | fzf)
+	hostname=$(awk '/Host / {print $2}' ~/.ssh/config | fzf)
+	BUFFER="rs $filename $hostname:/"
+	zle end-of-line
+}
+zle -N rs_push
+bindkey "^u" rs_push
 
-	function rs_push() {
-        filename=$(ls | fzf)
-        hostname=$(cat ~/.ssh/config | grep "Host "| fzf | awk {'print $2'})
-        BUFFER="rs $filename $hostname:/"
-		zle end-of-line
-	}
-	zle -N rs_push
-	bindkey "^u" rs_push
+# Pull file using rsync
+function rs_pull() {
+	hostname=$(awk '/Host / {print $2}' ~/.ssh/config | fzf)
+	BUFFER="rs $hostname:/"
+	zle end-of-line
+}
+zle -N rs_pull
+bindkey "^y" rs_pull
 
-	function rs_pull() {
-        hostname=$(cat ~/.ssh/config | grep "Host "| fzf | awk {'print $2'})
-        BUFFER="rs $hostname:/"
-		zle end-of-line
-	}
-	zle -N rs_pull
-	bindkey "^y" rs_pull
-
-# Command
-	function remote_config(){
-		BUFFER="~/.remote_setup.sh"
-		zle accept-line
-	}
-	zle -N remote_config
-	bindkey "^z" remote_config
+# Run remote setup script
+function remote_config() {
+	BUFFER="~/.remote_setup.sh"
+	zle accept-line
+}
+zle -N remote_config
+bindkey "^z" remote_config
