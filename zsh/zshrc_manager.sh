ec2d93254317cc6f17080b4b68dddbd30138aeb1
anh vo <anhvth.226@gmail.com>
2025-04-14 21:44:45 +0700

    update

--- zsh/zshrc_manager.sh
+++ zsh/zshrc_manager.sh
@@ -1,5 +1,5 @@
 time_out () { perl -e 'alarm shift; exec @ARGV' "$@"; }
-
-source ~/dotfiles/zsh/zshrc.sh
+source $HOME/dotfiles/zsh/mygroup.sh
+source $HOME/dotfiles/zsh/zshrc.sh
 export PATH=$PATH:~/dotfiles/bin
 
