289fa7e355dcb7f53691adb09beb89bde8d42718
anhvth <anhvth.226@gmail.com>
2025-11-16 12:56:54 +0700

    update

--- zsh/zshrc_manager.sh
+++ zsh/zshrc_manager.sh
@@ -1,3 +1,17 @@
 time_out () { perl -e 'alarm shift; exec @ARGV' "$@"; }
 source $HOME/dotfiles/zsh/zshrc.sh
 export PATH=$PATH:~/dotfiles/bin
+
+# Lazy conda loading to avoid startup overhead
+conda() {
+    unset -f conda
+    source "/mnt/disk1/anhvth8/miniconda3/etc/profile.d/conda.sh"
+    conda "$@"
+}
+
+# Optional: lazy activate function
+cact() {
+    source "/mnt/disk1/anhvth8/miniconda3/etc/profile.d/conda.sh"
+    conda activate "$@"
+}
+
